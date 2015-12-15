be5f7c6a4d5ddd0b96bf4ab6d93ca68d6b851df6
Sankho Mallik <smallik@nsone.net>
2015-12-15 16:06:27 -0500

    generalizes rest_resources for use w/ other resources... hopefully

--- test/utils/rest_resource_tests.js
+++ test/utils/rest_resource_tests.js
@@ -5,7 +5,6 @@ let expect     = require('chai').expect,
     utils      = require('./'),
     pluralize  = require('pluralize')
 
-let total_objects = 0
 
 module.exports = function(options) {
 
@@ -19,7 +18,11 @@ module.exports = function(options) {
     update_key
   } = options
     
-  let class_name         = `NS1.${options.subject.name}`
+  let class_name       = `NS1.${options.subject.name}`,
+      new_object_attrs = {},
+      total_objects    = 0
+
+  new_object_attrs[new_object_key] = new_object_val
 
   describe(`${class_name}.find()`, () => {
     it(`Should return all relevant objects in an array`, () => {
@@ -55,7 +58,7 @@ module.exports = function(options) {
     it(`Should update a resource's value`, () => {
       let object
 
-      return NS1.Zone.find(existing_val).then((_object) => {
+      return subject.find(existing_val).then((_object) => {
         object                       = _object
         old_update_attrs[update_key] = object.attributes[update_key]
 
@@ -64,35 +67,32 @@ module.exports = function(options) {
         expect(new_object).to.deep.equal(object)
         expect(new_object.attributes.refresh).to.eq(update_val)
 
-        return NS1.Zone.find(existing_val)
+        return subject.find(existing_val)
       }).then((object_check) => {
         expect(object_check.attributes[update_key]).to.eq(update_val)
       })
     })
   })
 
-  describe(`NS1.Zone#save() by way of NS1.Zone.create(), then NS1.Zone#destroy(), temporary janky testing`, () => {
-    it(`Should create a brand new zone, then destroy it`, () => {
-      let zone
+  describe(`${class_name}#save() by way of ${class_name}.create(), then ${class_name}#destroy(), temporary janky testing`, () => {
+    it(`Should create a brand new ${class_name}, then destroy it`, () => {
+      let object
 
-      return NS1.Zone.create({ 
-        zone: new_object_val 
-      }).then((z) => {
-        zone = z
-        expect(zone.attributes.zone).to.eq(new_object_val)
-        expect(zone.attributes.ttl).to.eq(3600)
+      return subject.create(new_object_attrs).then((_object) => {
+        object = _object
+        expect(object.attributes[new_object_key]).to.eq(new_object_val)
 
-        return NS1.Zone.find()
-      }).then((zones) => {
-        expect(zones.length).to.eq(total_objects + 1)
+        return subject.find()
+      }).then((objects) => {
+        expect(objects.length).to.eq(total_objects + 1)
 
-        return zone.destroy()
+        return object.destroy()
       }).then((destroyed) => {
         expect(destroyed).to.eq(true)
 
-        return NS1.Zone.find()
-      }).then((zones) => {
-        expect(zones.length).to.eq(total_objects)
+        return subject.find()
+      }).then((objects) => {
+        expect(objects.length).to.eq(total_objects)
       })
     })
   })
