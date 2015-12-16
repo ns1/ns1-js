c02ffcdff09bd670bf5f9062735d6eb0bd1cae7f
Sankho Mallik <smallik@nsone.net>
2015-12-16 18:41:07 -0500

    adds record and record test files

---
+++ test/record_test.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let expect = require('chai').expect,
+    NS1    = require('../lib'),
+    utils  = require('./utils')
+
+utils.setup_context('NS1.Record', function() {
+  /*
+  utils.rest_resource_tests.call(this, {
+    subject:        NS1.Record,
+    existing_val:   'testdomain.test',
+    existing_key:   'zone',
+    new_object_val: 'newtestdomain.test',
+    new_object_key: 'zone',
+    update_val:     665,
+    update_key:     'refresh'
+  })
+  //*/
+})
