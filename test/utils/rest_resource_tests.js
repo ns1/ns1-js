6b9f18fd1f4aba9c57032e3bc456f8dcc769ec94
Sankho Mallik <smallik@nsone.net>
2015-12-16 18:41:35 -0500

    wrapper around rest resource tests

--- test/utils/rest_resource_tests.js
+++ test/utils/rest_resource_tests.js
@@ -24,75 +24,77 @@ module.exports = function(options) {
 
   new_object_attrs[new_object_key] = new_object_val
 
-  describe(`${class_name}.find()`, () => {
-    it(`Should return all relevant objects in an array`, () => {
-      return subject.find().then((objects) => {
-        expect(Array.isArray(objects)).to.eq(true)
-        expect(objects[0].constructor).to.eq(subject)
-        total_objects = objects.length
+  describe('Should behave resoucefully', function() {
+    describe(`${class_name}.find()`, function() {
+      it(`Should return all relevant objects in an array`, function() {
+        return subject.find().then((objects) => {
+          expect(Array.isArray(objects)).to.eq(true)
+          expect(objects[0].constructor).to.eq(subject)
+          total_objects = objects.length
+        })
+      })
+
+      it (`Should return a single object when an ID is specified`, function() {
+        return subject.find(existing_val).then((object) => {
+          expect(typeof object).to.eq('object')
+          expect(Array.isArray(object)).to.eq(false)
+          expect(object.constructor).to.eq(subject)
+          expect(object.attributes[existing_key]).to.eq(existing_val)
+        })
       })
     })
 
-    it (`Should return a single object when an ID is specified`, () => {
-      return subject.find(existing_val).then((object) => {
-        expect(typeof object).to.eq('object')
-        expect(Array.isArray(object)).to.eq(false)
-        expect(object.constructor).to.eq(subject)
-        expect(object.attributes[existing_key]).to.eq(existing_val)
+    describe(`${class_name}#update()`, function() {
+      let old_update_attrs = {},
+          new_update_attrs = {}
+          
+      new_update_attrs[update_key] = update_val
+
+      after(function() {
+        return subject.find(existing_val).then((object) => {
+          return object.update(old_update_attrs)
+        })
+      })
+
+      it(`Should update a resource's value`, function() {
+        let object
+
+        return subject.find(existing_val).then((_object) => {
+          object                       = _object
+          old_update_attrs[update_key] = object.attributes[update_key]
+
+          return object.update(new_update_attrs)
+        }).then((new_object) => {
+          expect(new_object).to.deep.equal(object)
+          expect(new_object.attributes.refresh).to.eq(update_val)
+
+          return subject.find(existing_val)
+        }).then((object_check) => {
+          expect(object_check.attributes[update_key]).to.eq(update_val)
+        })
       })
     })
-  })
 
-  describe(`${class_name}#update()`, () => {
-    let old_update_attrs = {},
-        new_update_attrs = {}
-        
-    new_update_attrs[update_key] = update_val
+    describe(`${class_name}#save() by way of ${class_name}.create(), then ${class_name}#destroy(), temporary janky testing`, function() {
+      it(`Should create a brand new ${class_name}, then destroy it`, function() {
+        let object
 
-    after(() => {
-      return subject.find(existing_val).then((object) => {
-        return object.update(old_update_attrs)
-      })
-    })
+        return subject.create(new_object_attrs).then((_object) => {
+          object = _object
+          expect(object.attributes[new_object_key]).to.eq(new_object_val)
 
-    it(`Should update a resource's value`, () => {
-      let object
+          return subject.find()
+        }).then((objects) => {
+          expect(objects.length).to.eq(total_objects + 1)
 
-      return subject.find(existing_val).then((_object) => {
-        object                       = _object
-        old_update_attrs[update_key] = object.attributes[update_key]
+          return object.destroy()
+        }).then((destroyed) => {
+          expect(destroyed).to.eq(true)
 
-        return object.update(new_update_attrs)
-      }).then((new_object) => {
-        expect(new_object).to.deep.equal(object)
-        expect(new_object.attributes.refresh).to.eq(update_val)
-
-        return subject.find(existing_val)
-      }).then((object_check) => {
-        expect(object_check.attributes[update_key]).to.eq(update_val)
-      })
-    })
-  })
-
-  describe(`${class_name}#save() by way of ${class_name}.create(), then ${class_name}#destroy(), temporary janky testing`, () => {
-    it(`Should create a brand new ${class_name}, then destroy it`, () => {
-      let object
-
-      return subject.create(new_object_attrs).then((_object) => {
-        object = _object
-        expect(object.attributes[new_object_key]).to.eq(new_object_val)
-
-        return subject.find()
-      }).then((objects) => {
-        expect(objects.length).to.eq(total_objects + 1)
-
-        return object.destroy()
-      }).then((destroyed) => {
-        expect(destroyed).to.eq(true)
-
-        return subject.find()
-      }).then((objects) => {
-        expect(objects.length).to.eq(total_objects)
+          return subject.find()
+        }).then((objects) => {
+          expect(objects.length).to.eq(total_objects)
+        })
       })
     })
   })
